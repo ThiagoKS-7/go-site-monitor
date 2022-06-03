34342d83c72ab8de756a89eb7a812b0b5767c2f5
Thiago Kasper de Souza <thiago.souza@meta.com.br>
2022-06-03 18:39:29 -0300

    #1-release: implementando estrutura de pastas e divisão de packages

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	run "workspace/pkg"
+)
+
+/*
+	PROJETO DE MONITORAMENTO DE SISTEMAS EM GOLANG
+	Exemplo da aula introdutória de go
+	Consegue:
+		-Iniciar monitoramento dos sites;
+		-Exibir logs dos sites;
+*/
+func main() {
+	run.Welcome(run.GetNameAndVersion())
+}
+
